fba2d7895d99a3382ae958384acfaf7dc953e20a
Nitya <nmp79@cornell.edu>
2023-11-08 20:46:14 -0500

    changed name to simon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="grid grid-cols-3 gap-x-4 gap-y-8">
         <InfoCard
           imageURL="https://m.media-amazon.com/images/I/41j7lA0rK5L._AC_UY1000_.jpg"
-          avatarName="Nitya"
+          avatarName="Simon"
           avatarNetID="nmpxx"
           description="Found this let me know if you want it!!!" 
           datetime={new Date()}
